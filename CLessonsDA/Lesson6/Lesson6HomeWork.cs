0bc8935df13ef60505ad5386c0b52c030d6baf81
vakar <vakarmak@gmail.com>
2023-11-26 16:11:37 +0200

    lesson 6 task 2

--- CLessonsDA/Lesson6/Lesson6HomeWork.cs
+++ CLessonsDA/Lesson6/Lesson6HomeWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CLessonsDA.Lesson6
+﻿namespace CLessonsDA.Lesson6
 {
     internal class Lesson6HomeWork
     {
@@ -14,7 +8,7 @@ namespace CLessonsDA.Lesson6
             // Дано два числа A и B (A < B) выведите суму всех чисел расположенных между данными числами на экран.
             // Дано два числа A и B (A < B) выведите все нечетные значения, расположенные между данными числами.
 
-            
+
         }
 
         public static void Task2()
@@ -28,7 +22,78 @@ namespace CLessonsDA.Lesson6
             //· равносторонний треугольник   
             //· ромб
 
+            Console.WriteLine("Прямоугольник");
+            Console.WriteLine();
+            for (int rectangle = 0; rectangle < 4; rectangle++)
+            {
+                for (int rectangle2 = 0; rectangle2 < 10; rectangle2++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Прямоугольный треугольник");
+            Console.WriteLine();
+            for (int rightTriangle = 0; rightTriangle < 10; rightTriangle++)
+            {
+                for (int rightTriangle2 = 0; rightTriangle2 < rightTriangle; rightTriangle2++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Ромб");
+            Console.WriteLine();
 
+            // rSize - изначальный размер ромба, используется для контролирования кол-ва строк, в которых рисуются пробелы или *
+            // line - линия, в которой рисуется пробел или *
+            // indentation - отступ, программа построена прежде всего на отступах, так как для того чтобы нарисовать ромб сначала нужно отступить на несколько символов,
+            // если в цикле указать начальное значение не 0, а 1 то нарисованная фигура в конечном итоге сместиться влево на один символ.
+            // Программа построена таким образом, что рисуется сначала верхняя часть, а потом нижняя.
+            // Условие indentation < 2 * line - 1 обеспечивает рисование с двух сторон, если число 2 заменить на 1, то полученный результат будет похож на половину ромба,
+            // при чем отрисована будет только левая его часть, данное условие применяется только в части отрисовывания * так как после завершения данной операции следует переход на новую строку.
+            // Console.Write("\n"); в данном случае используется намеренно после отрисовывания * так как нужен переход на новую строку.
+            // Отрисовывание середины ромба происходит на самой последней иттерации 2-го цикла ("*") где условно обозначена верхняя часть ромба. 
+
+            int rSize = 5;
+            
+            // верхняя часть ромба
+            
+            for (int line = 1; line <= rSize; line++)
+            {
+                for (int indentation = 0; indentation < rSize - line; indentation++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int indentation = 0; indentation < 2 * line - 1; indentation++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
+            
+            //нижняя часть ромба
+
+            for (int line = rSize - 1; line >= 1; line--)
+            {
+                for (int indentation = 0; indentation < rSize - line; indentation++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int indentation = 0; indentation < 2 * line - 1; indentation++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
         }
 
         public static void Task3()
--- CLessonsDA/RunProject.cs
+++ CLessonsDA/RunProject.cs
@@ -24,8 +24,8 @@
             //Lesson5.Lesson5HomeWork.Task5();
 
             //Lesson6.Lesson6HomeWork.Task1();
-            //Lesson6.Lesson6HomeWork.Task2();
-            Lesson6.Lesson6HomeWork.Task3();
+            Lesson6.Lesson6HomeWork.Task2();
+            //Lesson6.Lesson6HomeWork.Task3();
 
             RunProject.LecturePractice();
         }
